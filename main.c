b1a6fa517e78e5948b2286e1ea33052967d01099
brblisko <vesely.svk@gmail.com>
2022-02-19 16:25:27 +0100

    created function that calculates CPU usage

--- main.c
+++ main.c
@@ -1,6 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
+
+bool calcCpuUsage(float *cpuUsage)
+{
+    char buffer[10000];
+    FILE *fp = fopen("/proc/stat", "r");
+    if (fp == NULL)
+    {
+        fprintf(stderr, "ERROR - cannot open file \"/proc/stat\"\n");
+        return false;
+    }
+
+    if (fgets(buffer, 10000, fp) == NULL)
+    {
+        fprintf(stderr, "ERROR - cannot read file \"/proc/stat\"\n");
+        return false;
+    }
+    fclose(fp);
+    char *token = strtok(buffer, " ");
+    float sum = 0, idle = 0;
+    int i = 0;
+    while (token != NULL)
+    {
+        token = strtok(NULL, " ");
+        if (token != NULL)
+        {
+            sum += atoi(token);
+        }
+        if (i == 3)
+        {
+            idle = atoi(token);
+        }
+
+        i++;
+    }
+
+    *cpuUsage = 100 - ((idle * 100) / sum);
+
+    return true;
+}
 
 bool checkargs(int argc, char **argv, int *port)
 {
@@ -23,13 +63,16 @@ bool checkargs(int argc, char **argv, int *port)
 int main(int argc, char **argv)
 {
     int port = 0;
+    float cpuUsage = 0;
 
-    if (!checkargs(argc, argv, &port))
+    if (!checkargs(argc, argv, &port) ||
+        !calcCpuUsage(&cpuUsage))
     {
         return 1;
     }
 
-    printf("%d", port);
+    printf("%d\n", port);
+    printf("%f\n", cpuUsage);
 
     return 0;
 }
