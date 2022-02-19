25282ca95bf46976cc24f59773679522ca8028a5
brblisko <vesely.svk@gmail.com>
2022-02-20 00:06:36 +0100

    added server support and re did cpu load function

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <stdbool.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 bool fetchCpuName(char *cpuName)
 {
@@ -39,70 +41,62 @@ bool fetchHostName(char *hostName)
         fprintf(stderr, "ERROR - cannot read file \"/proc/sys/kernel/hostname\"\n");
         return false;
     }
+    fclose(fp);
 
     return true;
 }
 
-bool calcCpuUsage(float *cpuUsage)
+bool calcCpuUsage(int *cpuUsage)
 {
-    char buffer[10000];
-    FILE *fp = fopen("/proc/stat", "r");
-    if (fp == NULL)
-    {
-        fprintf(stderr, "ERROR - cannot open file \"/proc/stat\"\n");
-        return false;
-    }
-    float prevSum = 0, prevIdle = 0;
-    float sum = 0, idle = 0;
-
+    int data[2][10];
     for (int i = 0; i < 2; i++)
     {
-        float tmpSum = 0, tmpIdle = 0;
+        FILE *fp = fopen("/proc/stat", "r");
+        if (fp == NULL)
+        {
+            fprintf(stderr, "ERROR - cannot open file \"/proc/stat\"\n");
+            return false;
+        }
+        char buffer[10000];
+        int j = 0;
+
         if (fgets(buffer, 10000, fp) == NULL)
         {
             fprintf(stderr, "ERROR - cannot read file \"/proc/stat\"\n");
             return false;
         }
+
         char *token = strtok(buffer, " ");
-        int j = 0;
 
         while (token != NULL)
         {
             token = strtok(NULL, " ");
-            if (token != NULL && j != 3 && j != 4)
+            if (token != NULL)
             {
-                tmpSum += atof(token);
+                data[i][j] = atoi(token);
             }
-            if (j == 3 || j == 4)
-            {
-                tmpIdle = atof(token);
-            }
-
             j++;
-            if (j == 8)
-            {
-                break;
-            }
         }
 
-        if (i == 0)
-        {
-            prevSum = tmpSum;
-            prevIdle = tmpIdle;
-        }
-        if (i == 1)
-        {
-            sum = tmpSum;
-            idle = tmpIdle;
-        }
+        fclose(fp);
         sleep(1);
     }
-    fclose(fp);
 
-    float totalD = 0, idleD = 0;
-    totalD = (idle + sum) - (prevIdle + prevSum);
-    idleD = idle - prevIdle;
-    *cpuUsage = 100 - (((totalD - idleD) / totalD) * 100);
+    int idle[2] = {0, 0};
+    int total[2] = {0, 0};
+
+    for (int i = 0; i < 2; i++)
+    {
+        idle[i] = data[i][3] + data[i][4];
+        for (int j = 0; j < 10; j++)
+        {
+            total[i] += data[i][j];
+        }
+    }
+    int totaldif = total[1] - total[0];
+    int idledif = idle[1] - idle[0];
+
+    *cpuUsage = 100 * (totaldif - idledif) / totaldif;
 
     return true;
 }
@@ -128,21 +122,81 @@ bool checkargs(int argc, char **argv, int *port)
 int main(int argc, char **argv)
 {
     int port = 0;
-    float cpuUsage = 0;
+    int cpuUsage = 0;
     char hostName[100], cpuName[1000];
 
-    if (!checkargs(argc, argv, &port) ||
-        !calcCpuUsage(&cpuUsage) ||
-        !fetchHostName(hostName) ||
-        !fetchCpuName(cpuName))
+    if (!checkargs(argc, argv, &port))
     {
         return 1;
     }
 
-    printf("%d\n", port);
-    printf("%f\n", cpuUsage);
-    printf("%s", hostName);
-    printf("%s", cpuName + 1); // we need to skip first character due to it being " "
+    int mySocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    int reuse = 1;
+    setsockopt(mySocket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, (const char *)&reuse, sizeof(int));
+    struct sockaddr_in server;
+    server.sin_family = AF_INET;
+    server.sin_port = htons(port);
+    server.sin_addr.s_addr = INADDR_ANY;
+    bind(mySocket, (struct sockaddr *)&server, sizeof(server));
+    listen(mySocket, 256);
+
+    while (true)
+    {
+
+        int clientFd = accept(mySocket, NULL, NULL);
+        char buffer[10000];
+        if (read(clientFd, buffer, 10000) == -1)
+        {
+            fprintf(stderr, "ERROR");
+            return 1;
+        }
+
+        if (!strncmp(buffer, "GET /hostname", 13))
+        {
+            if (!fetchHostName(hostName))
+            {
+                return 1;
+            }
+
+            char response[] = "HTTP/1.1 200 OK\r\nContent-Type: text/plain;\r\n\r\n";
+
+            write(clientFd, response, strlen(response));
+            write(clientFd, hostName, strlen(hostName));
+        }
+        else if (!strncmp(buffer, "GET /cpu-name", 13))
+        {
+            if (!fetchCpuName(cpuName))
+            {
+                return 1;
+            }
+            char response[10000];
+            strcpy(response, "HTTP/1.1 200 OK\r\nContent-Type: text/plain;\r\n\r\n");
+            strcat(response, cpuName + 1);
+
+            write(clientFd, response, strlen(response));
+        }
+        else if (!strncmp(buffer, "GET /load", 9))
+        {
+            if (!calcCpuUsage(&cpuUsage))
+            {
+                return 1;
+            }
+            char tmp[80];
+            sprintf(tmp, "%d", cpuUsage);
+            char response[10000];
+            strcpy(response, "HTTP/1.1 200 OK\r\nContent-Type: text/plain;\r\n\r\n");
+            strcat(response, tmp);
+            strcat(response, " %\n");
+
+            write(clientFd, response, strlen(response));
+        }
+        else
+        {
+            char response[] = "HTTP/1.1 200 OK\r\nContent-Type: text/plain;\r\n\r\n400 Bad Request";
+            write(clientFd, response, sizeof(response) - 1);
+        }
+        close(clientFd);
+    }
 
     return 0;
 }
